8ca4cae90a635598616c18adedce6737b1afa1f2
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2020-08-01 19:30:19 -0400

    Update 161904130_180600810_a04.c

--- 161904130_180600810_a04.c
+++ 161904130_180600810_a04.c
@@ -1,8 +1,22 @@
+/*
+CP386 Assignment 4
+Wilfrid Laurier University
 
+Franchesco Livado
+Student ID: 161904130
+Github: Franchesco Livado
 
-#include<stdio.h>
+Anthony Youkhanna
+Student ID: 180600810
+Github: Anthony Youkhanna
+
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <unistd.h>
 
 //constants
 int rows = 5;
@@ -15,7 +29,7 @@ int r1;
 int r2;
 int r3;
 int r4;	
-
+int safe[5];
 //list of commands
 
 char RQ[2];
@@ -27,6 +41,7 @@ char run[3];
 typedef struct available
 {
 	int avArray[4];
+	int new[4];
 } Available;
 
 typedef struct maximum
@@ -46,15 +61,18 @@ typedef struct need{
 typedef struct thread{
     int numOfReq;
     int tNum;
+	int safe;
     //pthread_t handle;
 } Threads;
 
 void readFile(char* fileName, Max *max);
 void commandRun(char *c);
 int resourceReq(int *tNum, int *r1, int *r2, int *r3, int *r4);
-void resourceRelease(int *tNum, int *r1, int *r2, int *r3, int *r4);
+int resourceRelease(int *tNum, int *r1, int *r2, int *r3, int *r4);
 void printState();
 void fillAlloc();
+void* threadRun(void* t);
+void createSafe();
 
 //inits of structs
 Threads* threads;
@@ -81,6 +99,7 @@ int main(int argc, char *argv[])
 	printf("Currently Available resources: ");
 	for (int i = 1; i < argc; i++){
 		available.avArray[c] = atoi(argv[i]);
+
 		printf("%d ", atoi(argv[i]));
 		
 		c++;
@@ -91,8 +110,9 @@ int main(int argc, char *argv[])
 	readFile("sample4_in.txt", &max);
 	printf("Maximum resources from file: \n");
 	for (int i = 0; i < rows; i++){
-        threads[i].tNum = 0;
+        threads[i].tNum = i;
         threads[i].numOfReq = 0;
+		threads[i].safe = 0;
 		for (int j = 0; j < columns; j++){
 			printf("%d ", max.mArray[i][j]);
 		}
@@ -103,11 +123,12 @@ int main(int argc, char *argv[])
 	printf("Enter a command : ");
 	//command is string for commands: RQ, RL
 	scanf("%s", command);
-	//cmd is string for command: *
-	strncpy(cmd, command, 1);
-	//runcmd is string for command: Run
-	strncpy(runcmd, command, 3);
+	
 	while (strcmp(command, "Run") != 0){
+		//cmd is string for command: *
+		strncpy(cmd, command, 1);
+		
+
 		if (strcmp(command, "*") != 0 && strcmp(command, "Run") != 0){
 			scanf("%d %d %d %d %d", &threadNum, &r1, &r2, &r3, &r4);
 			printf("command entered\n");
@@ -120,13 +141,14 @@ int main(int argc, char *argv[])
 
 		printf("Enter a command : ");
 		scanf("%s", command);
+		//runcmd is string for command: Run
+		strncpy(runcmd, command, 3);
 	}
 
-	if(strcmp(runcmd, "Run") == 0){
-			commandRun(runcmd);
-	}
+	createSafe();
+	commandRun(runcmd);
 	
-
+	return 0;
 }
 
 
@@ -141,7 +163,7 @@ void readFile(char* fileName, Max *max)
 		printf("Child A: Error in opening input file...exiting with error code -1\n");
 	}
 	else{
-		    const char s[1] = ",";
+		//const char s[1] = ",";
 		char line[100];
 		char *token;
 		int col;
@@ -174,23 +196,43 @@ int resourceReq(int *tNum, int *r1, int *r2, int *r3, int *r4){
         //check if available <= (max - request) AKA Safety algorithm or if (max - request) is negative
         for(int i = 0; i < columns; i++){
 		
-            if (i == 0 && ((max.mArray[*tNum][i] - *r1) <= -1 || (max.mArray[*tNum][i] - *r1) > available.avArray[i])){
-                printf("i: %d \n(max.mArray[*tNum][i] - *r1): %d - %d \n", i, max.mArray[*tNum][i], *r1);
+            if ((i == 0 && ((max.mArray[*tNum][i] - *r1) <= -1)) || (available.avArray[i] - *r1) <= 0){// (max.mArray[*tNum][i] - *r1) > available.avArray[i]) || 
+                //printf("i: %d \n(max.mArray[*tNum][i] - *r1): %d - %d \n", i, max.mArray[*tNum][i], *r1);
+				
 				return -1;					
             }
-            if (i == 1 && ((max.mArray[*tNum][i] - *r2) > available.avArray[i] || (max.mArray[*tNum][i] - *r2) <= -1)){
-                printf("i: %d \n(max.mArray[*tNum][i] - *r2): %d - %d \n", i, max.mArray[*tNum][i], *r2);
+            if ((i == 1 && ((max.mArray[*tNum][i] - *r2) <= -1)) || (available.avArray[i] - *r2) <= 0){//(max.mArray[*tNum][i] - *r2) > available.avArray[i] || 
+                //printf("i: %d \n(max.mArray[*tNum][i] - *r2): %d - %d \n", i, max.mArray[*tNum][i], *r2);
+				
 				return -1;				
             }
-            if (i== 2 && ((max.mArray[*tNum][i] - *r3) > available.avArray[i] || (max.mArray[*tNum][i] - *r3) <= -1)){
-				printf("i: %d \n(max.mArray[*tNum][i] - *r3): %d - %d \n", i, max.mArray[*tNum][i], *r3);
+            if ((i== 2 && ((max.mArray[*tNum][i] - *r3) <= -1)) || (available.avArray[i] - *r3) <= 0){ //(max.mArray[*tNum][i] - *r3) > available.avArray[i] || 
+				//printf("i: %d \n(max.mArray[*tNum][i] - *r3): %d - %d \n", i, max.mArray[*tNum][i], *r3);
+				
 				return -1;
             }
-            if (i == 3 && ((max.mArray[*tNum][i] - *r4) > available.avArray[i] || (max.mArray[*tNum][i] - *r4) <= -1)){
-                printf("i: %d \n(max.mArray[*tNum][i] - *r4): %d - %d \n", i, max.mArray[*tNum][i], *r4);
+            if ((i == 3 && ((max.mArray[*tNum][i] - *r4) <= -1)) || (available.avArray[i] - *r4) <= 0){ //(max.mArray[*tNum][i] - *r4) > available.avArray[i] ||
+                //printf("i: %d \n(max.mArray[*tNum][i] - *r4): %d - %d \n", i, max.mArray[*tNum][i], *r4);
+				
 				return -1;
             }
         }
+		for (int i = 0; i < columns; i++){
+			if (i == 0){
+				available.avArray[i] = available.avArray[i] - *r1;
+			}
+			if (i == 1){
+				available.avArray[i] = available.avArray[i] - *r2;
+			}
+			if (i == 2){
+				available.avArray[i] = available.avArray[i] - *r3;
+			}
+			if (i == 3){
+				available.avArray[i] = available.avArray[i] - *r4;
+			}
+		}
+		
+		
 		threads[*tNum].numOfReq ++;
         return 0;
     }
@@ -201,20 +243,70 @@ int resourceReq(int *tNum, int *r1, int *r2, int *r3, int *r4){
 
 }
 
-void resourceRelease(int *tNum, int *r1, int *r2, int *r3, int *r4){
+void fillNeed(){
+	for (int j = 0; j < rows; j++){
 
+		for (int i = 0; i < columns; i++){
+
+			need.needArray[j][i] = max.mArray[j][i] - allocated.allocArray[j][i];
+			
+		}
+		
+	}
+	
 }
 
-//safety algorithm
-// 1.let work = available and finish[i] =  false for each resource
-// 2.find an index such that both
-// 	finish[i] == false
-// 	need[i] <= work
-// if no such i exists go to step 4
-// 3.work = work + allocation[i]
-//   finish[i] = true
-//   go to step 2
-// 4.if finish[i] == true for all i, then system is safe. else state is unsafe.
+int resourceRelease(int *tNum, int *r1, int *r2, int *r3, int *r4){
+	if (threads[*tNum].numOfReq > 0){
+        for(int i = 0; i < columns; i++){
+			//checks if there a resources available to release
+            if (i == 0 && ((allocated.allocArray[*tNum][i] - *r1) <= -1)){
+				
+				return -1;					
+            }
+            if (i == 1 && ((allocated.allocArray[*tNum][i] - *r2) <= -1)){
+				
+				return -1;				
+            }
+            if (i== 2 && ((allocated.allocArray[*tNum][i] - *r3) <= -1)){ 
+				
+				return -1;
+            }
+            if (i == 3 && ((allocated.allocArray[*tNum][i] - *r4) <= -1)){ 
+				
+				return -1;
+            }
+        }
+		//adds resources back to available and removes resources from the allocated
+		for (int i = 0; i < columns; i++){
+			if (i == 0){
+				available.avArray[i] = available.avArray[i] + *r1;
+				allocated.allocArray[*tNum][i] = allocated.allocArray[*tNum][i] - *r1;
+			}
+			if (i == 1){
+				available.avArray[i] = available.avArray[i] + *r2;
+				allocated.allocArray[*tNum][i] = allocated.allocArray[*tNum][i] - *r2;
+
+			}
+			if (i == 2){
+				available.avArray[i] = available.avArray[i] + *r3;
+				allocated.allocArray[*tNum][i] = allocated.allocArray[*tNum][i] - *r3;
+			}
+			if (i == 3){
+				available.avArray[i] = available.avArray[i] + *r4;
+				allocated.allocArray[*tNum][i] = allocated.allocArray[*tNum][i] - *r4;
+			}
+		}
+
+		threads[*tNum].numOfReq --;
+        return 0;
+    }
+    else{
+		
+        return -1;
+    }
+
+}
 
 
 void printState(){
@@ -272,7 +364,7 @@ void fillAlloc(){
 
 void commandRun(char *c){
 
-
+	int b;
 	//state - * output current state of data structs
 	if (strcmp(c, "*") == 0){
 		
@@ -281,10 +373,11 @@ void commandRun(char *c){
 	}
 	//request
 	else if (strcmp(c, "RQ") == 0){
-		int b = resourceReq(&threadNum, &r1, &r2, &r3, &r4);
+		b = resourceReq(&threadNum, &r1, &r2, &r3, &r4);
 		if (b == 0){
             //fill alloc array then print
             fillAlloc();
+			fillNeed();
             printf("Request satisfied \n");
         }
         else{
@@ -293,13 +386,133 @@ void commandRun(char *c){
 	}
 	//release
 	else if (strcmp(c, "RL") == 0){
-		resourceRelease(&threadNum, &r1, &r2, &r3, &r4);
-		
+		b = resourceRelease(&threadNum, &r1, &r2, &r3, &r4);
+		if (b == 0){
+            printf("Request satisfied \n");
+        }
+        else{
+            printf("Request denied for one of the following reasons:\n	state would be unsafe\n	not enough resources\n	thread does not exist\n");
+        }
 	}
 	//run - implements safe sequence and runs threads
 	else if (strcmp(c, "Run") == 0){
-		
+		int bool = 0;
+		int i=0;
+		while(i < rows)//put a suitable condition here to run your program
+			{
+				int tnum = safe[i];
+				pthread_attr_t attr;
+				pthread_t tid = threads[tnum].tNum;
+				pthread_attr_init(&attr);
+				bool = pthread_create(&tid, &attr, threadRun, &threads[tnum]);
+				sleep(5);
+				pthread_join(tid, NULL);
+
+				if (bool == 0){
+					i++;
+				}
+				//write suitable code here to run the threads
+			}
 
 	}
+
+}
+
+void* threadRun(void* t){
+
+	int tid = ((Threads*)t)->tNum;
+	printf("--> Customer/Thread %d \n", tid);
+	printf("	Allocated resources:	");
+	for (int j = 0; j < columns; j++)
+	{
+		printf("%d ", allocated.allocArray[tid][j]);
+	}
+		
+	printf("\n	Needed:		");
+	for (int j = 0; j < columns; j++)
+	{
+		printf("%d ", need.needArray[tid][j]);
+	}	
+	
+	printf("\n	Available:	");
+	for (int j = 0; j < columns; j++)
+	{
+		printf("%d ", available.avArray[j]);
+	}
+
+	printf("\n	Thread has started \n");
+	printf("	Thread has finished \n");
+	printf("	Thread is releasing resources \n");
+
+	printf("	New Available: ");
+
+
+
+	for (int j = 0; j < columns; j++)
+	{
+		available.avArray[j] = available.avArray[j] + allocated.allocArray[tid][j];
+	}
+
+
+
+
+	for (int j = 0; j < columns; j++)
+	{
+		printf("%d ", available.avArray[j]);
+	}
+	printf("\n");
+	pthread_exit(0);
+}
+
+void createSafe(){
+	//need <= work then work = work + allocation and add to safe sequence
+	for (int i = 0; i < columns; i++)
+	{
+		available.new[i] =  available.avArray[i];
+	}
+	
+	int added = 0;
+	int check = 0;
+	while (added < rows)
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			if (threads[i].safe == 0)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					if (need.needArray[i][j] <= available.new[j])
+					{
+						check = 1;
+					}else
+					{
+						check = 0;
+						break;
+					}
+				}
+			}	
+			if (check == 1)
+			{
+				printf("thread: %d", threads[i].tNum);
+				threads[i].safe = 1;
+				safe[added]= threads[i].tNum;
+				added++;
+
+				for (int j = 0; j < columns; j++)
+				{
+					available.new[j] = available.new[j] + allocated.allocArray[i][j];
+				}	
+			}
+			check = 0;
+		}
+	}
+	printf("\n Safe sequence is: <");
+	for (int i = 0; i < 5; i++)
+	{
+		printf(" %d", safe[i]);
+	}
+	printf(" >\n");
+	
+
 
 }
